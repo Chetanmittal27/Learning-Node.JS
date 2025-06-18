dc22ce0973a46976592f86751070345310e86423
Chetan <chetanmittal39@gmail.com>
2025-06-18 19:15:21 +0530

    Frontend Backend Connection

--- Basic Project/frontend/src/App.jsx
+++ Basic Project/frontend/src/App.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useEffect } from 'react';
 
 function App(){
 
   const [jokes , setJokes] = useState([]);
 
-useEffect(() => {
-  axios.get('https://localhost:3000/jokes')
+  useEffect(() => {
+    axios.get('/api/jokes')
     .then((response) => setJokes(response.data))
     .catch((error) => console.log(error));
-}, []);
+  } , [])
 
 
-  return (
-    <div className='flex  flex-col justify-center items-center h-[100vh] bg-black text-white'>
-      <h1 className='text-6xl font-bold'>Hello World</h1>
-      <p className='text-4xl font-semibold p-7'>Jokes : {jokes.length}</p>
+  return(
+    <div className='bg-black text-white flex justify-center h-[100vh] flex-col items-center'>
+      <h1 className='text-6xl font-bold'>Hello World!</h1>
+      <p className='text-4xl font-semibold p-6'>Jokes : {jokes.length}</p>
 
       {jokes.map((joke , index) => (
         <div key={joke.id}>
-          <h2>{joke.title}</h2>
-          <p>{joke.content}</p>
+          <h3 className='text-3xl p-3'>{joke.title}</h3>
+          <p className='text-2xl p-3'>{joke.content}</p>
         </div>
       ))}
     </div>
